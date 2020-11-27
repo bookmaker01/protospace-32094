a0a7e646fa18cdaee1a01ff88f26ce03a8f17e6a
bookmaker01 <booker4423@gmail.com>
2020-11-28 03:01:23 +0900

    third　commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  
 
 
   def index
@@ -31,7 +31,6 @@ class PrototypesController < ApplicationController
     end
 
     def edit
-      @prototype = Prototype.find(params[:id])
     end
 
     def update
@@ -45,9 +44,11 @@ class PrototypesController < ApplicationController
     end
 
     def destroy
-      prototype = Prototype.find(params[:id])
-      prototype.destroy
-      render :index
+      if @prototype.destroy
+        redirect_to root_path
+      else
+        redirect_to root_path
+      end
     end
 
   private
